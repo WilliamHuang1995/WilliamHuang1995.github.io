a7fb11be2f0f7c5fca45225f03f6e52609b40d52
William Huang <wjhuang@ucsd.edu>
2016-11-06 22:03:21 +0800

    p5 is so cool

--- sketch.js
+++ sketch.js
@@ -1,17 +1,15 @@
 function setup() {
-  createCanvas(640,640);
+  createCanvas(640,400);
 
 }
 
 function draw() {
-  background(255,125,0);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
-  fill(255,0,0,150);
-  rect(240,145,20,100);
-  fill(0,0,255,215);
-  ellipse(240,115,60,60);
-  fill(255,0,0);
+  //background
+  background(250,250,100);
+  
+  //ellipse
+  fill(250,200,200);
+  ellipse(mouseX, mouseY, 100,100);
   
 
 }
