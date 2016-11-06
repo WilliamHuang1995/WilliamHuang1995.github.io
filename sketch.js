e01af658df84590772a7776fa241d51797a16d89
William Huang <wjhuang@ucsd.edu>
2016-11-06 21:23:57 +0800

    second commit, just trying stuff atm

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(640,640);
+
+}
+
+function draw() {
+  background(255,125,0);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+  fill(255,0,0,150);
+  rect(240,145,20,100);
+  fill(0,0,255,215);
+  ellipse(240,115,60,60);
+  fill(255,0,0);
+  
+
+}
