0b41d668ff2071f6c07d3e75d3781177999dfac2
William Huang <shsidforever@gmail.com>
2017-11-28 23:19:15 +0800

    added initial typed js (fluff)

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import Typed from '../node_modules/typed.js/src/typed.js';
+
+var options = {
+    strings: ['the Cool','the Smart','the Generous'],
+    typeSpeed: 30,
+    backSpeed: 1000,
+    fadeOut: true,
+    loop: true
+}
+var typed = new Typed("#typed", options);
